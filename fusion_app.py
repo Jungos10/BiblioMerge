c07be87ed9232d7aa819b7c18b7f334528320213
Jungos10 <diezjunguitu@gmail.com>
2025-05-05 07:55:47 +0200

    Add files via upload

---
+++ fusion_app.py
@@ -0,0 +1,10 @@
+
+# fusion_app.py
+import streamlit as st
+import pandas as pd
+import io
+
+# Configuración inicial de la app
+st.set_page_config(page_title="Fusionador Scopus + WoS", layout="centered")
+
+# ... (el resto del código ya estaba guardado previamente en el editor)
