06443e5baa9012506ee71702725ce5193dd5af9f
Jungos10 <diezjunguitu@gmail.com>
2025-05-15 17:37:00 +0200

    Update app (BUENO DÍA 13 A LAS 7h).py

--- app (BUENO DÍA 13 A LAS 7h).py
+++ app (BUENO DÍA 13 A LAS 7h).py
@@ -38,7 +38,7 @@ st.markdown("""
 
     .recursos-cabecera {
         position: absolute;
-        top: 1.2rem;
+        top: 2.5rem;
         right: 25rem;
         text-align: left;
         font-size: 0.95rem;
