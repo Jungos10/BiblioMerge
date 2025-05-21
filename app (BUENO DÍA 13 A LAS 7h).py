e58d67920ce946c37b9e80992c80e038197d3b71
Jungos10 <diezjunguitu@gmail.com>
2025-05-21 20:31:56 +0200

    Update app (BUENO DÍA 13 A LAS 7h).py

--- app (BUENO DÍA 13 A LAS 7h).py
+++ app (BUENO DÍA 13 A LAS 7h).py
@@ -15,9 +15,10 @@ st.set_page_config(page_title="BiblioMerge", layout="wide")
 
 st.markdown("""
 <style>
-body {
+html, body, .main {
     margin-top: 0 !important;
     padding-top: 0 !important;
+}
 }
 .cabecera-sticky {
     position: fixed;
