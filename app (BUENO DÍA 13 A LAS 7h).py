4cdaf4fd9bda19ca1c1f08a383b2f35973e3f678
Jungos10 <diezjunguitu@gmail.com>
2025-05-24 18:58:09 +0200

    Update app (BUENO DÍA 13 A LAS 7h).py

--- app (BUENO DÍA 13 A LAS 7h).py
+++ app (BUENO DÍA 13 A LAS 7h).py
@@ -1163,7 +1163,18 @@ with col2:
             st.write(f"**Registros finales:** {df_final.shape[0]}")
         
             # Contar elementos únicos en cada campo
-            num_autores = len(set(";".join(df_final["Authors"].dropna()).split(";")))
+            #num_autores = len(set(";".join(df_final["Authors"].dropna()).split(";")))
+
+            autores_limpios = (
+                df_final["Authors"]
+                .dropna()
+                .str.split(";")
+                .explode()
+                .str.strip()
+            )
+
+            num_autores = autores_limpios.nunique()
+
             num_author_keywords = len(set(";".join(df_final["Author Keywords"].dropna()).split(";")))
             num_index_keywords = len(set(";".join(df_final["Index Keywords"].dropna()).split(";")))
             num_references = len(set(";".join(df_final["References"].dropna()).split(";")))
