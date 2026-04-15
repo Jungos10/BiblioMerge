ee84b8c7c5473c604e79f5ad5b4ae64e82f7a0f8
Jungos10 <diezjunguitu@gmail.com>
2026-04-15 17:56:35 +0200

    Update fillna to target only text columns
    Refactor fillna method to apply to text columns only.

--- BMPueba22Feb26.py
+++ BMPueba22Feb26.py
@@ -270,7 +270,9 @@ if st.session_state.get("fusion_en_proceso", False):
     
         dfwos_selected['Source'] = 'WOS'
         df_concatenated = pd.concat([dfsco, dfwos_selected], ignore_index=True)
-        df_concatenated.fillna('', inplace=True)
+        # df_concatenated.fillna('', inplace=True)
+        text_cols = df_concatenated.select_dtypes(include=['object', 'string']).columns
+        df_concatenated[text_cols] = df_concatenated[text_cols].fillna('')
     
         df_concatenated['EID'] = (
             df_concatenated['EID']
